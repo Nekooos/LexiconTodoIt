2c162c292fe5e57d9397983103884d7aca0d2c34
UsagiDrops <kristoffer011235813@gmail.com>
2021-10-11 10:48:45 +0200

    Person class and Person class tests

---
+++ TodoIt/Model/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIt.Model
+{
+    public class Person
+    {
+        private readonly int personId;
+        private String firstName;
+        private String lastName;
+
+        public Person(int personId, String firstName, String lastName)
+        {
+            this.personId = personId;
+            this.firstName = NullOrEmpty(firstName);
+            this.lastName = NullOrEmpty(lastName);
+        }
+
+        public int PersonId
+        {
+            get => personId;
+        }
+
+        public String FirstName
+        {
+            get => firstName;
+            set
+            {
+                firstName = NullOrEmpty(value);
+            }
+        }
+
+        public String LastName
+        {
+            get => lastName; 
+            set 
+            {
+                lastName = NullOrEmpty(value);
+            }
+        }
+
+        private String NullOrEmpty(String value)
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Value can't be null or empty");
+            }
+            return value;
+        }
+    }
+}
---
+++ TodoItTests/Model/PersonTests.cs
@@ -0,0 +1,92 @@
+using System;
+using TodoIt.Model;
+using Xunit;
+
+namespace TodoItTests
+{
+    public class PersonTests
+    {
+
+        private Person testPerson;
+
+        public PersonTests()
+        {
+            testPerson = new Person(1, "firstName", "lastName");
+        }
+
+        [Fact]
+        public void SetLastNameNullTest()
+        {
+            Assert.Throws<ArgumentException>(() => testPerson.LastName = null);
+        }
+
+        [Fact]
+        public void SetFirstNameNullTest()
+        {
+            Assert.Throws<ArgumentException>(() => testPerson.FirstName = null);
+        }
+
+        [Fact]
+        public void SetLastNameEmptyTest()
+        {
+            Assert.Throws<ArgumentException>(() => testPerson.LastName = String.Empty);
+        }
+
+        [Fact]
+        public void SetFirstNameEmptyTest()
+        {
+            Assert.Throws<ArgumentException>(() => testPerson.FirstName = String.Empty);
+        }
+
+        [Fact]
+        public void ConstructorValueEmptyTest()
+        {
+            Assert.Throws<ArgumentException>(() => new Person(2, null, "lastName"));
+        }
+
+        [Fact]
+        public void GetIdTest()
+        {
+            int personId = testPerson.PersonId;
+            int expectedPersonId = 1;
+
+            Assert.Equal(expectedPersonId, personId);
+        }
+
+        [Fact]
+        public void GetFirstNameTest()
+        {
+            String firstName = testPerson.FirstName;
+            String expectedFirstName = "firstName";
+
+            Assert.Equal(expectedFirstName, firstName);
+        }
+
+        [Fact]
+        public void GetLastNameTest()
+        {
+            String lastName = testPerson.LastName;
+            String expectedLastName = "lastName";
+
+            Assert.Equal(expectedLastName, lastName);
+        }
+
+        [Fact]
+        public void SetFirstNameTest()
+        {
+            testPerson.FirstName = "newFirstName";
+            String expectedFirstName = "newFirstName";
+
+            Assert.Equal(expectedFirstName, testPerson.FirstName);
+        }
+
+        [Fact]
+        public void SetLastNameTest()
+        {
+            testPerson.LastName = "newLastName";
+            String expectedLastName = "newLastName";
+
+            Assert.Equal(expectedLastName, testPerson.LastName);
+        }
+    }
+}
